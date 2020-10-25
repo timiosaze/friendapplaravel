19df3209cfd0c659fa8cca8e3b74fc2801ca5e74
timiosaze <timiade1993@gmail.com>
2020-10-25 14:07:47 +0100

    Brand link changed

--- routes/web.php
+++ routes/web.php
@@ -11,15 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('auth.login');
-});
-// Route::get('/myfriends', function(){
-//     return view('app.myfriends');
-// });
-// Route::get('/editmyfriends', function(){
-//     return view('app.editmyfriends');
-// });
+
 
 Route::get('/friends', 'FriendController@index')->name('friend.index');
 Route::post('/friends', 'FriendController@store')->name('friend.store');
@@ -28,4 +20,4 @@ Route::put('/friends/{id}', 'FriendController@update')->name('friend.update');
 Route::delete('/friends/{id}', 'FriendController@destroy')->name('friend.destroy');
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'HomeController@index')->name('home');
